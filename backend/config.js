5d847cb0b2276785c365c98db6068bf287b15c33
Ratheshan Sathiyamoorthy <37710130+Ratheshan03@users.noreply.github.com>
2024-03-04 09:19:12 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://Ratheshan03:rathe03@atlascluster.bzmeyoe.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
+  "mongodb+srv://<user>:<ps>@atlascluster.bzmeyoe.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
